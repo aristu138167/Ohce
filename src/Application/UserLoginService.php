4a2aa2a310cc0cdd440f22a87dbe135c132e22c4
Urki Aristu <aristu.138167@e.unavarra.es>
2023-03-07 12:44:02 +0000

    Anadiendo UserLoginServiceDoubles

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -2,13 +2,36 @@
 
 namespace UserLoginService\Application;
 
+use Exception;
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
+
 class UserLoginService
 {
     private array $loggedUsers = [];
+    private FacebookSessionManager $facebookSessionManager;
 
-    public function manualLogin(): string
+    /**
+     * @throws Exception
+     */
+    public function manualLogin(User $user): void
     {
-        return "user logged";
+        if(in_array($user,$this->loggedUsers)) {
+            throw new Exception('User alredy logged in');
+        }
+        else{
+            $this->loggedUsers[]=$user;
+        }
+    }
+
+    public function getLoggerUsers(): array{
+        return $this->loggedUsers;
+    }
+
+    public function getExternalSessions(): int
+    {
+        $facebookSessionManager= new FacebookSessionManager();
+        return $facebookSessionManager.getSessions();
     }
 
 }
--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -4,18 +4,53 @@ declare(strict_types=1);
 
 namespace UserLoginService\Tests\Application;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function userIsManuallyLoggedIn()
+    {
+        $userLoginService = new UserLoginService();
+        $user=new User("name");
+
+        $userLoginService->manualLogin($user);
+
+        $this->assertContains($user,$userLoginService->getLoggerUsers());
+    }
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function errorWhileLoginUserIfIsAlredyLoggedIn()
+    {
+        $userLoginService = new UserLoginService();
+        $user=new User("name");
+
+
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('User alredy logged in');
+
+        $userLoginService->manualLogin($user);
+        $userLoginService->manualLogin($user);
+    }
+
+    /**
+     * @test
+     */
+    public function xUsersInExternalSessions()
     {
         $userLoginService = new UserLoginService();
 
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $userLoginService->getExternalSessions();
+
+        $this->assert
     }
 }
